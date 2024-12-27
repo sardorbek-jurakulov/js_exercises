72367efaf5001c244cd083a2c73e514e38f42192
“sardorbek-jurakulov” <sardorbek.juraqulov@gmail.com>
2024-12-27 15:12:21 +0500

    4 tasks in exercise 1 file have been completed

--- src/exercise_1.js
+++ src/exercise_1.js
@@ -1,5 +1,5 @@
 /**
- * Write a function to check if a string is empty 
+ * Write a function to check if a string is empty
  * @param {String} text
  * @returns {Boolean}
  * @example
@@ -9,26 +9,54 @@
  * isStringEmpty(); => throws error "text must be defined"
  */
 function isStringEmpty(text) {
-  // Your code here
+  if (text === undefined) {
+    return "text must be defined";
+  } else if (text.trim().length > 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 /**
  * Write a function to truncate text
- * @param {String} text 
+ * @param {String} text
  * @param {Number} numberOfCharacters
- * @returns {String} 
+ * @returns {String}
  * @example
  * truncateString('Hello World', 2); => 'He'
  * truncateString('Hello world'); => throws error "Please specify number of characters to extract"
  * truncateString(''); => throws error "text must have at least one character"
  */
 function truncateString(text, numberOfCharacters) {
-  // Your code here
+  if (text === undefined) {
+    return "text must be defined";
+  }
+
+  if (text === '') {
+    return "text must have at least one character";
+  }
+
+  if (numberOfCharacters === undefined) {
+    return "Please specify number of characters to extract";
+  } else if (numberOfCharacters < 0) {
+    return "The value indicating how many characters to extract must be a natural number";
+  }
+
+  // first solution
+  let truncatedText = ''
+  for (let i = 0; i < numberOfCharacters; i++ ) {
+    truncatedText += text[i];
+  }
+  return truncatedText;
+
+  // second solution
+  // return text.slice(0, numberOfCharacters);
 }
 
 /**
  * Write a function to create social media post hash tag
- * @param {String} text 
+ * @param {String} text
  * @returns {String}
  * @example
  * createHashTag('Hello World'); => '#helloWorld'
@@ -38,12 +66,25 @@ function truncateString(text, numberOfCharacters) {
  * createHashTag('   '); => throws error "Text should have at least three characters"
  */
 function createHashTag(text) {
-  // Your code here
+  if (text === undefined || text.trim() === '') {
+    return "Text should have at least three characters";
+  }
+
+  const textAsArray = text.split(" ");
+  textAsArray[0] = textAsArray[0].charAt(0).toLowerCase() + textAsArray[0].slice(1, textAsArray[0].length);
+
+  const textAsArrayLength = textAsArray.length;
+  for (let i = 1; i < textAsArrayLength; i++) {
+    textAsArray[i] = textAsArray[i].charAt(0).toUpperCase() + textAsArray[i].slice(1, textAsArray[i].length);
+  }
+
+  text = textAsArray.join('');
+  return '#' + text;
 }
 
 /**
  * Write a function to format phone number as '+998 99 777 66 55'
- * @param {Number} phoneNumber 
+ * @param {Number} phoneNumber
  * @returns {String}
  * @throws {Error} 'Phone number must be either 9 or 12 characters long'
  * @example
@@ -54,19 +95,35 @@ function createHashTag(text) {
  * formatPhoneNumber(); => throws error "Phone number must be either 9 or 12 characters long"
  */
 function formatPhoneNumber(phoneNumber) {
-  // Your code here
+  if (phoneNumber === undefined || typeof phoneNumber !== 'number') {
+    // return "Phone number must be either 9 or 12 characters long";
+    throw "Phone number must be either 9 or 12 characters long";
+  } else if (phoneNumber.toString().length > 12 || phoneNumber.toString().length < 9) {
+    // return "Phone number must be either 9 or 12 characters long";
+    throw "Phone number must be either 9 or 12 characters long";
+  }
+
+  phoneNumber = phoneNumber.toString();
+  if (phoneNumber.length === 12) {
+    return `+${phoneNumber.slice(0, 3)} ${phoneNumber.slice(3, 5)} ${phoneNumber.slice(5, 8)} ${phoneNumber.slice(8, 10)} ${phoneNumber.slice(10, 12)}`
+  } else if (phoneNumber.length === 9) {
+    return `+998 ${phoneNumber.slice(0, 2)} ${phoneNumber.slice(2, 5)} ${phoneNumber.slice(5, 7)} ${phoneNumber.slice(7, 9)}`
+  } else {
+    // return "Phone number must be either 9 or 12 characters long";
+    throw "Phone number must be either 9 or 12 characters long";
+  }
 }
 
 /**
  * Write a function that transforms text to different cases
- * @param {String} text 
+ * @param {String} text
  * @param {'camel'|'kebab'|'snake'} caseName - 'camel', 'kebab', 'snake'
  * @returns {String}
  * @example
  * changeTextCase('Hello World', 'camel'); => 'helloWorld'
  * changeTextCase('Hello World', 'kebab'); => 'hello-world'
  * changeTextCase('Hello World', 'snake'); => 'hello_world'
- * 
+ *
  */
 function changeTextCase(text, caseName) {
   // Your code here
@@ -91,7 +148,7 @@ function replaceWordInText(text, word, replacement) {
 
 /**
  * Write a function to extract price in number format from the text
- * @param {String} text 
+ * @param {String} text
  * @returns {Number}
  * @example
  * extractPriceFromText('Apple price in market is $2.32 per kg now'); => 2.32
